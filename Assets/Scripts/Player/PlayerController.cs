391c2482a77c3f05873b85b60cdcfa4100fe57bb
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2026-08-23 10:23:10 +0700

    feat: player movement + environment setup + trail

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private float acceleration = 50f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        moveInput = InputManager.instance.GetPlayerMovement();
+    }
+    
+    private void HandleMovement()
+    {
+        Vector2 targetVelocity = moveInput * moveSpeed;
+
+        rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity, targetVelocity, acceleration * Time.deltaTime);
+    }
+}
